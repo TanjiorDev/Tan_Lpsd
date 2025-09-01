ad959bf6f26803e96a31afd99096fb4ee3c8dd69
TanjiorDev <168908351+TanjiorDev@users.noreply.github.com>
2025-09-01 23:10:48 +0200

    Update policeserver.lua

--- Tan_Lpsd/server/policeserver.lua
+++ Tan_Lpsd/server/policeserver.lua
@@ -5,7 +5,20 @@ TriggerEvent('esx_phone:registerNumber', 'police', 'alerte police', true, true)
 TriggerEvent('esx_society:registerSociety', 'police', 'police', 'society_police', 'society_police', 'society_police', {type = 'public'})
 
 
-local ESX = exports["es_extended"]:getSharedObject()
+RegisterServerEvent('ox_inventory:openInventory')
+AddEventHandler('ox_inventory:openInventory', function(type, targetId)
+    local src = source
+    local target = tonumber(targetId)
+    if not target then return end
+
+    -- Vérifie que le joueur source est BCSO et en service
+    local xPlayer = ESX.GetPlayerFromId(src)
+    if not xPlayer then return end
+    if xPlayer.job.name ~= 'police' or not xPlayer.job.onDuty then return end
+
+    -- Ouvre l'inventaire du joueur ciblé pour le joueur source
+    TriggerClientEvent('ox_inventory:openInventory', src, type, target)
+end)
 
 -- Event : réception d’une demande de RDV
 RegisterNetEvent("rdv:sendToPolice")
